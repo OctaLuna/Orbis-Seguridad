1785cfaa4a94b8916f701cdebcca5aa138c2d8b6
pupi1000 <einar.guillen@ucb.edu.bo>
2026-04-30 00:36:09 -0400

    cambio en la estructura de revistapage tanto en funcionalidad como en diseño para una vista mas clara y evitar accesos no autorizados

--- Frontend/src/screens/revistaPage.jsx
+++ Frontend/src/screens/revistaPage.jsx
@@ -4,24 +4,24 @@ import "./revistaPage.css";
 export default function RevistaPage() {
   return (
     <div className="revista-container">
-      <div className="revista-frame">
-        <div className="revista-hover-area">
-          <img
-            src="/media/revista/revista.png" // reemplaza con tu imagen real
-            alt="Portada Revista"
-            className="revista-img"
-          />
-          <div className="revista-tooltip">
-            Revista desarrollada por la carrera de Ing. de Sistemas<br />
-            de la Universidad Católica Boliviana
-          </div>
-          <p className="revista-footer-text">
-        Edición especial — Universidad Católica Boliviana © 2025
-      </p>
+      <div className="revista-divider"></div>
       
+      <div className="revista-hover-area">
+        <img
+          src="/media/revista/revista.png"
+          alt="Portada Revista"
+          className="revista-img"
+        />
+        <div className="revista-tooltip">
+          Revista desarrollada por la carrera de Ing. de Sistemas
+          <br />
+          de la Universidad Católica Boliviana
         </div>
       </div>
+
+      <p className="revista-footer-text">
+        Edición especial — Universidad Católica Boliviana © 2026
+      </p>
     </div>
-    
   );
 }
